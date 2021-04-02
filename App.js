de4b0cfc70a6d1828851f93e3de67653f4a4e337
tradecoder <thetradecoder@gmail.com>
2021-04-02 21:42:43 +0600

    local update

--- App.js
+++ App.js
@@ -9,12 +9,27 @@ export default function App() {
   const [users, setUsers] =  useState([]);
   const [chat, setChat] = useState([]);
   const [isLoggedin, setIsLoggedin] = useState(false);
+
+  function onPressCreateRoom(){
+    // redirect to a form to Create a Room
+  }
+  function onPressJoinChat(){
+    //redirect to to a form to Join a Chat
+  }
   return (
     <SafeAreaProvider>
       <ThemeProvider theme={theme}>
-        <Grid>
+        <Grid style={{maxWidth:'400px'}}>
           <Row>
-            <Text>AnoChat</Text>
+            <Text h1>AnoChat</Text>
+          </Row>
+          <Row style={{display:'block'}}>
+            <Text>Create a Chat Room and send the id to whom you want to join.</Text>
+            <Button type='outline'  title="Create a Chat Room" onPress={onPressCreateRoom}/>
+          </Row>
+          <Row style={{display:'block'}}>
+            <Text>Or join a room.</Text>
+            <Button type='outline'  title="Join a Chat Room" onPress={onPressJoinChat}/>
           </Row>
       </Grid>   
     </ThemeProvider>
